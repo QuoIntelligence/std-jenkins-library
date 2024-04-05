7bc9b0ac175b005d616edcd5872d0c4fe9520dcd
Juanjo Presa <juanjo.presa@nadanix.com>
2024-04-05 21:15:42 +0200

    Update stdDiscover.groovy

--- vars/stdDiscover.groovy
+++ vars/stdDiscover.groovy
@@ -1,7 +1,6 @@
 #!/usr/bin/env groovy
 
 def call() {
-    stage('Discover') {
       env.TMP_DIR = pwd(tmp:true)
 
       env.SKIP_DRV_EXPORT = false;
@@ -12,5 +11,4 @@ def call() {
       loadResourceScript "eval.sh"
       sh "bash ./eval.sh"
       sh "grep ^json $GITHUB_OUTPUT | cut -d \"=\" -f2 > \$TMP_DIR/outputs.json"
-  }
 }
