2bee63ef5f0993ccb8bb95554ab08f6427794834
Juanjo Presa <juanjo.presa@quointelligence.eu>
2024-07-29 00:23:50 +0200

    Update stdDiscover.groovy

--- vars/stdDiscover.groovy
+++ vars/stdDiscover.groovy
@@ -10,7 +10,6 @@ def call() {
       env.GITHUB_OUTPUT = "${env.TMP_DIR}/outputs"
       env.SKIP_DRV_EXPORT = false
       env.flake_url = "."
-      env.discover_apply = "x: x"
 
       sh "rm -rf \$EVALSTORE_EXPORT \$GITHUB_OUTPUT"
       sh "bash ./eval.sh"
